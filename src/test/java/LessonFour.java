212981ae261bb26f21f364d61b03f405d67c3715
Serg Sh. <vampir6307@gmail.com>
2024-10-17 08:09:54 +0300

    LessonFourСorrections

--- src/test/java/LessonFour.java
+++ src/test/java/LessonFour.java
@@ -5,12 +5,11 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 
+
 public class LessonFour {
 
-
-
     @Test
-            void tests(){
+    void searchTest(){
         open("https://github.com/selenide/selenide");
         //открыть https://github.com/selenide/selenide
         $("#wiki-tab").click();
@@ -20,11 +19,21 @@ public class LessonFour {
         //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $$("ul.m-0.p-0.list-style-none li").get(20).$("a").click();
         //Откройте страницу SoftAssertions
-        $(".markdown-body").shouldHave(Condition.text("@ExtendWith({SoftAssertsExtension.class})"));
+        $(".markdown-body").shouldHave(Condition.text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
         //Проверьте что внутри есть пример кода для JUnit5
-        //(".markdown-heading").$("heading-element").shouldHave(Condition.text("How to soft assert using Selenide")); Не понимаю почему это не работает ((
+        //(".markdown-heading").$("heading-element").shouldHave(Condition.text("@ExtendWith({SoftAssertsExtension.class})")); Не понимаю почему это не работает ((
 //$(".markdown-body").shouldHave(Condition.text("@ExtendWith({SoftAssertsExtension.class})")); а это работает, объясните пожалуйста.
 
     }
-
 }
+
